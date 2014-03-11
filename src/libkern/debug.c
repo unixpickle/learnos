6bf6ec5fb25f84e01e1d35a401d0c7a80f946d4e
Alex Nichol <unixpickle@gmail.com>
2014-03-10 21:03:41 -0400

    added missing debug sources

---
+++ src/libkern/debug.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void ensure_critical() {
+  uint64_t flags;
+  __asm__ ("pushfq\npop %0" : "=r" (flags));
+  if (flags & (1 << 9)) {
+    die("ZONE IS NOT CRITICAL\n");
+  }
+}
+
+void print_stack() {
+  uint64_t rsp;
+  __asm__ ("mov %%rsp, %0" : "=r" (rsp));
+  print("stack is ");
+  printHex(rsp);
+  print(" for CPU ");
+  printHex(lapic_get_id());
+  print("\n");
+}
+
---
+++ src/libkern/debug.h
@@ -0,0 +1,3 @@
+void ensure_critical();
+
+
