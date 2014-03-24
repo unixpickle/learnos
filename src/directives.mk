3177249fb2f8f378785b71259cebd55a2ae5d92f
Alex Nichol <unixpickle@gmail.com>
2014-03-24 15:39:54 -0400

    fixed makefile

--- src/directives.mk
+++ src/directives.mk
@@ -1,5 +1,5 @@
 
 DISABLE_OPTIONS=-Wall -ffreestanding -fno-builtin -fno-stack-protector -mno-red-zone
 DESTARCH=elf64-x86-64
-INCLUDES_64=-I$(PROJECT_ROOT)/src -I$(PROJECT_ROOT)/src/libkern -I$(PROJECT_ROOT)/libs/anpages/src -I$(PROJECT_ROOT)/libs/anlock/src -I$(PROJECT_ROOT)/libs/anidxset/src -I$(PROJECT_ROOT)/libs/anscheduler/include -I$(PROJECT_ROOT)/src/smp/include
+INCLUDES_64=-I$(PROJECT_ROOT)/src -I$(PROJECT_ROOT)/src/libkern -I$(PROJECT_ROOT)/libs/anpages/src -I$(PROJECT_ROOT)/libs/anlock/src -I$(PROJECT_ROOT)/libs/anidxset/src -I$(PROJECT_ROOT)/libs/anscheduler/include -I$(PROJECT_ROOT)/src/scheduler/include
 INCLUDES_32=-I$(PROJECT_ROOT)/src
