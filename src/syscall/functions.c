417095d2c50ddbd6a80492edd7cf4ec27070c60f
Alex Nichol <unixpickle@gmail.com>
2014-03-26 13:10:51 -0400

    actually make the syscalls *callable*

--- src/syscall/functions.c
+++ src/syscall/functions.c
@@ -21,6 +21,24 @@ uint64_t syscall_entry(uint64_t arg1,
     syscall_sleep(arg2);
   } else if (arg1 == 3) {
     syscall_exit();
+  } else if (arg1 == 4) {
+    syscall_thread_exit();
+  } else if (arg1 == 5) {
+    syscall_wants_interrupts();
+  } else if (arg1 == 6) {
+    return syscall_get_interrupts();
+  } else if (arg1 == 7) {
+    return syscall_open_socket();
+  } else if (arg1 == 8) {
+    return syscall_connect(arg2, arg3);
+  } else if (arg1 == 9) {
+    syscall_close_socket(arg2);
+  } else if (arg1 == 10) {
+    return syscall_write(arg2, arg3, arg4);
+  } else if (arg1 == 11) {
+    return syscall_read(arg2, arg3);
+  } else if (arg1 == 12) {
+    return syscall_poll();
   }
   return 0;
 }
