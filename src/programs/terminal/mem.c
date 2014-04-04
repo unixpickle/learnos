590f82f1d9fc38548d9e114ad08ba0c3c99a1bfb
Alex Nichol <unixpickle@gmail.com>
2014-04-04 13:21:49 -0400

    suck away messages before memusage

--- src/programs/terminal/mem.c
+++ src/programs/terminal/mem.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 void command_memusage() {
+  // read away the command message first
+  int i = 2;
+  while (i) {
+    if (!sys_poll()) {
+      msg_t msg;
+      while (sys_read(0, &msg)) i--;
+    }
+  }
   printf("using %x pages.\n", sys_mem_usage());
   sys_exit();
 }
