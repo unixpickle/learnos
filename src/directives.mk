14b420f1ec5805d07e7afd6e76d717e76d22bf48
Alex Nichol <unixpickle@gmail.com>
2014-04-05 16:49:10 -0400

    it fails like i wanted it to

--- src/directives.mk
+++ src/directives.mk
@@ -1,4 +1,4 @@
-DISABLE_OPTIONS=-Wall -ffreestanding -fno-builtin -fno-stack-protector -mno-red-zone -fno-zero-initialized-in-bss
+DISABLE_OPTIONS=-Wall -ffreestanding -fno-builtin -fno-stack-protector -mno-red-zone -fno-zero-initialized-in-bss -nostdinc
 DESTARCH=elf64-x86-64
 INCLUDES_64=-I$(PROJECT_ROOT)/src -I$(PROJECT_ROOT)/src/libkern -I$(PROJECT_ROOT)/libs/anpages/src -I$(PROJECT_ROOT)/libs/anlock/src -I$(PROJECT_ROOT)/libs/anidxset/src -I$(PROJECT_ROOT)/libs/anscheduler/include -I$(PROJECT_ROOT)/src/scheduler/include -I$(PROJECT_ROOT)/libs/anmem/include -I$(PROJECT_ROOT)/libs/anmem/libs/analloc/src
 INCLUDES_32=-I$(PROJECT_ROOT)/src
--- src/programs/directives.mk
+++ src/programs/directives.mk
@@ -1,2 +1,2 @@
-CFLAGS += -Wall -ffreestanding -fno-builtin -fno-stack-protector -masm=intel -O2 -fno-zero-initialized-in-bss
+CFLAGS += -Wall -ffreestanding -fno-builtin -fno-stack-protector -masm=intel -O2 -fno-zero-initialized-in-bss -nostdinc
 INCLUDES=-I$(PROJECT_ROOT)/src/programs/libprog -I$(PROJECT_ROOT)/src/programs/libs/CKeyedBits/include
