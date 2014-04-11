dded5bcacd0d9e7f181d6bf3948be7bf08990bef
Alex Nichol <unixpickle@gmail.com>
2014-04-11 16:36:59 -0400

    outline for protocol

--- src/programs/memd/main.c
+++ src/programs/memd/main.c
@@ -1,11 +1,22 @@
 #include "client.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <keyedbits/buff_decoder.h>
+#include <keyedbits/validation.h>
 
 void handle_messages(uint64_t fd);
 void handle_faults();
-
 void handle_client_fault(client_t * cli, pgf_t * fault);
+
+const char * client_request(kb_buff_t * kb, uint64_t * start, uint64_t * count);
+void handle_client_request(client_t * cli,
+                           const char * action,
+                           uint64_t start,
+                           uint64_t count);
+bool handle_client_alloc(client_t * cli, uint64_t start, uint64_t count);
+bool handle_client_free(client_t * cli, uint64_t start, uint64_t count);
+void handle_client_death(client_t * cli);
 
 void main() {
   sys_become_pager();
@@ -23,6 +34,7 @@ void handle_messages(uint64_t fd) {
   msg_t msg;
   while (sys_read(fd, &msg)) {
     if (msg.type == 2) {
+      handle_client_death(cli);
       client_delete(cli);
       sys_close(fd);
       return;
@@ -31,9 +43,15 @@ void handle_messages(uint64_t fd) {
 
     kb_buff_t kb;
     kb_buff_initialize_decode(&kb, (void *)msg.message, msg.len);
-    
-    // TODO: parse the message here
-    printf("got client message, but who *really* cares, man\n");
+    uint64_t start, count;
+    const char * type = client_request(&kb, &start, &count);
+    if (!type) {
+      handle_client_death(cli);
+      client_delete(cli);
+      sys_close(fd);
+      return;
+    }
+    handle_client_request(cli, type, start, count);
   }
 }
 
@@ -50,12 +68,100 @@ void handle_faults() {
     handle_client_fault(client, &fault);
     sys_shift_fault();
   }
-  printf("done get faults.\n");
 }
 
 void handle_client_fault(client_t * cli, pgf_t * fault) {
   printf("client faulted because i'm a murderer.\n");
   // just murder it brutally cause i'm lazy
   sys_mem_fault(cli->pid);
+}
+
+const char * client_request(kb_buff_t * kb,
+                            uint64_t * start,
+                            uint64_t * count) {
+  const char * typeResult = NULL;
+  int found = 0;
+
+  kb_header_t header;
+  if (!kb_buff_read_header(kb, &header)) return NULL;
+  if (!kb_validate_header(&header)) return NULL;
+  if (header.typeField != KeyedBitsTypeDictionary) return NULL;
+
+  char key[32];
+  while (1) {
+    if (!kb_buff_read_key(kb, key, 32)) return NULL;
+    if (!key[0]) break;
+    found++;
+    if (!strcmp(key, "type")) {
+      if (!kb_buff_read_header(kb, &header)) return NULL;
+      if (!kb_validate_header(&header)) return NULL;
+      if (header.typeField != KeyedBitsTypeString) return NULL;
+      const char * value = 0;
+      uint64_t unused;
+      if (!kb_buff_read_string(kb, &value, &unused)) return NULL;
+      if (!strcmp(value, "alloc")) typeResult = "alloc";
+      else if (!strcmp(value, "free")) typeResult = "free";
+      else return NULL;
+    } else if (!strcmp(key, "start") || !strcmp(key, "count")) {
+      int64_t value;
+      if (!kb_buff_read_header(kb, &header)) return NULL;
+      if (!kb_validate_header(&header)) return NULL;
+      if (header.typeField != KeyedBitsTypeInteger) return NULL;
+      if (!kb_buff_read_int(kb, header.lenLen, &value)) return NULL;
+      if (value < 0) return NULL;
+      if (!strcmp(key, "start")) *start = (uint64_t)value;
+      else *count = (uint64_t)value;
+    } else {
+      return NULL;
+    }
+  }
+
+  if (found != 3) return NULL;
+  return typeResult;
+}
+
+void handle_client_request(client_t * cli,
+                           const char * action,
+                           uint64_t start,
+                           uint64_t count) {
+  bool res;
+  if (!strcmp(action, "alloc")) {
+    res = handle_client_alloc(cli, start, count);
+  } else if (!strcmp(action, "free")) {
+    res = handle_client_free(cli, start, count);
+  } else return;
+  if (!res) {
+    handle_client_death(cli);
+    sys_close(cli->fd);
+    client_delete(cli);
+  }
+}
+
+bool handle_client_alloc(client_t * cli, uint64_t start, uint64_t count) {
+  if (start != cli->pageCount) return false;
+  // TODO: grow the page array in cli->pages
+  uint64_t i;
+  for (i = 0; i < count; i++) {
+    // TODO: allocate the page here and map it
+  }
+  return true;
+}
+
+bool handle_client_free(client_t * cli, uint64_t start, uint64_t count) {
+  if (start > cli->pageCount) return false;
+  uint64_t i;
+  for (i = 0; i < count; i++) {
+    // TODO: unmap the page here
+  }
+  // TODO: send invlpg message here
+  for (i = 0; i < count; i++) {
+    // TODO: free the page here
+  }
+  // TODO: shrink the page array in cli->pages
+  return true;
+}
+
+void handle_client_death(client_t * cli) {
+  handle_client_free(cli, 0, cli->pageCount);
 }
 
