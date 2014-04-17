1196bd03d24584c84278a85d896f194658133c8a
Alex Nichol <unixpickle@gmail.com>
2014-04-17 11:48:16 -0400

    wrote pthread test

--- src/programs/terminal/threadtest.c
+++ src/programs/terminal/threadtest.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
+
+static void test_basic();
+static void * test_basic_thread(void * arg);
 
 void command_threadtest() {
-  printf("TODO: implement these\n");
+  printf("testing basic pthreads... ");
+  test_basic();
+  printf("passed!\n");
+  sys_exit();
+}
+
+static void test_basic() {
+  pthread_t thread;
+  int ret = pthread_create(&thread, NULL, test_basic_thread, (void *)0x1337);
+  assert(!ret);
+  void * retVal = NULL;
+  ret = pthread_join(thread, &retVal);
+  assert(!ret);
+  assert(retVal == (void *)~0x1337L);
+}
+
+static void * test_basic_thread(void * arg) {
+  return (void *)~(uint64_t)arg;
 }
 
